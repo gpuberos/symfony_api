e06d58060517b1b294c7db767f18f031f3083805
Guy-Philippe Uberos <gpuberos.pro@gmail.com>
2024-03-12 13:51:08 +0100

    add: createProduct

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,18 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): JsonResponse
+    #[Route('/product', name: 'create_product')]
+    public function createProduct(EntityManagerInterface $entityManager): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ProductController.php',
-        ]);
+        $product = new Product;
+        $product->setName('The Witcher');
+        $product->setPrice(7.99);
+        $product->setDescription('Incarnez le sorceleur Geralt de Riv, le légendaire tueur de monstre, happé dans une mystérieuse toile tissée par les forces luttant pour contrôler le monde. Prenez des décisions difficiles et assumez leurs conséquences dans un jeu qui vous plongera dans une aventure exceptionnelle.');
+
+        $entityManager->persist($product);
+
+        $entityManager->flush();
+
+        return new Response("Saved new product with id: {$product->getId()}");
     }
 }
